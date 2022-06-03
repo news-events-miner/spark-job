c529c9bb16b3e8dcb4b24e12e4b956f8404af157
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2022-06-03 23:41:29 +0500

    Add PySpark to dependencies

---
+++ spark_worker/__init__.py
@@ -0,0 +1,8 @@
+"""
+spark-event-extractor - simple spark job to extract events from
+text documents using topic modeling (Top2Vec)
+"""
+
+__version__ = "0.1.0"
+__date__ = "2022-06-03"
+__license__ = "MIT"
